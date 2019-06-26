30252b9a89cb6cc4a99554fa4b7c5d81ee40803d
tomoyukiHAYAKAWA <x14091xx@aitech.ac.jp>
2019-06-26 13:33:14 +0900

    年齢カウンターの実装完了

--- age.js
+++ age.js
@@ -1,21 +1,48 @@
 
-document.write("<p>getAge(1995, 4, 13)</p>")
+// document.write("<p>getAge(1995, 4, 13)</p>")
+//
+// function getAge(year, month, day){
+//
+//   //誕生年月日
+//   var birthday  = new Date(y, m-1, d);
+//
+//   //今日
+//   var today = new Date();
+//
+//   //今年の誕生日
+//   var thisYearBirthday =
+//       new Date(today.getFullYear(), birthday.getMonth(), birthday.getDate());
+//
+//   //今年-誕生年
+//   var age = today.getFullYear() - birthday.getFullYear();
+//
+//   //今年の誕生日を迎えていなければage-1を返す
+//   return (today < thisYearBirthday)?age-1:age;
+// }
 
-function getAge(year, month, day){
+// function setfig(num) {
+//    // 桁数が1桁だったら先頭に0を加えて2桁に調整する
+//    var ret;
+//    if( num < 10 ) { ret = "0" + num; }
+//    else { ret = num; }
+//    return ret;
+// }
+function ageCounter() {
 
-  //誕生年月日
-  var birthday  = new Date(y, m-1, d);
+  var duration = Date.now() - new Date("1995/4/13");
+  var age = duration / 1000 / 60 / 60 / 24 / 365;
+  age = age.toString().match(/\d{2}\.\d{9}/)[0];
 
-  //今日
-  var today = new Date();
+  const element = document.querySelector('.age_text');
+  element.innerText =  age
 
-  //今年の誕生日
-  var thisYearBirthday =
-      new Date(today.getFullYear(), birthday.getMonth(), birthday.getDate());
+  //document.getElementById("RealtimeClockArea").innerHTML =
+   // var birthday = new Data()
+   // var nowTime = new Date();
+   // var nowHour = setfig( nowTime.getHours() );
+   // var nowMin  = setfig( nowTime.getMinutes() );
+   // var nowSec  = setfig( nowTime.getSeconds() );
+   // var msg = "現在時刻は、" + nowHour + ":" + nowMin + ":" + nowSec + " です。";
 
-  //今年-誕生年
-  var age = today.getFullYear() - birthday.getFullYear();
-
-  //今年の誕生日を迎えていなければage-1を返す
-  return (today < thisYearBirthday)?age-1:age;
 }
+setInterval('ageCounter()', 30/1000);
