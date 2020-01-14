3d5e8e004fe8845b721566ccd05f1b30a205d858
tomoyukiHAYAKAWA <x14091xx@aitech.ac.jp>
2020-01-15 04:33:23 +0900

    色変更用の関数2つを宣言

---
+++ color.js
@@ -0,0 +1,16 @@
+
+var red;
+var green;
+var blue;
+
+function changeBackColor() {
+
+    red = Math.floor( Math.random() * 256 );
+    green = Math.floor( Math.random() * 256 );
+    blue = Math.floor( Math.random() * 256 );
+
+}
+
+function changeTextColor() {
+
+}
