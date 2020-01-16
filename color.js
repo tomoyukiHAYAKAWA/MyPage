f627b8584991f3d9d5b797edf871c498cd6cb24b
tomoyukiHAYAKAWA <x14091xx@aitech.ac.jp>
2020-01-17 02:40:52 +0900

    16進数に変換するコードと要素を指定して色を適用するっぽいコードを書いた

--- color.js
+++ color.js
@@ -5,12 +5,39 @@ var blue;
 
 function changeBackColor() {
 
+    const element = document.querySelector('wrapper');
+
     red = Math.floor( Math.random() * 256 );
     green = Math.floor( Math.random() * 256 );
     blue = Math.floor( Math.random() * 256 );
+
+    // 16進数にして返す
+    hex = rgb2hex(red, green, blue);
+
+    element.style.backgroundColor = hex;
 
 }
 
 function changeTextColor() {
 
+    const element = document.querySelector('wrapper');
+
+    let rgbArray = [red, green, blue];
+    let max = Math.max.apply(null, rgbArray);
+    let min = Math.min.apply(null, rgbArray);
+
+    let red = (max + min) - rgbArray[0];
+    let green = (max + min) - rgbArray[1];
+    let blue = (max + min) - rgbArray[2];
+
+    hex = rgb2hex(red, green, blue);
+
+    element.style.color = hex;
+
+}
+
+function rgb2hex ( rgb ) {
+	return "#" + rgb.map( function ( value ) {
+		return ( "0" + value.toString( 16 ) ).slice( -2 ) ;
+	} ).join( "" ) ;
 }
